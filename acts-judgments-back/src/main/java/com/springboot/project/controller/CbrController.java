e2962855c07b17205f452a98e2e2fc1c3868e4f9
SrdjanTopic <64950418+SrdjanTopic@users.noreply.github.com>
2023-04-02 17:02:03 +0200

    getting list of judgment names from backend

--- acts-judgments-back/src/main/java/com/springboot/project/controller/CbrController.java
+++ acts-judgments-back/src/main/java/com/springboot/project/controller/CbrController.java
@@ -19,6 +19,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -149,6 +151,24 @@ public class CbrController  {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @GetMapping("judgments")
+    public List<String> getJudgmentNames(){
+        List<String> results = new ArrayList<String>();
+
+        try {
+            File[] files = ResourceUtils.getFile("classpath:judgments/").listFiles();
+            assert files != null;
+            for (File file : files) {
+                if (file.isFile()) {
+                    results.add(file.getName());
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return results;
     }
 
     @GetMapping("/acts/{actName}")
